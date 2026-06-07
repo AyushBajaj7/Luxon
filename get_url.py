1b52718103da01db19a9a3d1ff3b0838540a296f
Ayush Bajaj <ayushbajaj215@gmail.com>
2026-06-07 18:09:17 +0545

    chore: Remove temporary test scripts and test images

--- get_url.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import psycopg
-
-load_dotenv('d:/projects/Luxon/.env')
-conn = psycopg.connect(os.environ['DATABASE_URL'])
-cur = conn.cursor()
-cur.execute("SELECT image_url FROM products WHERE name LIKE '%Caviar%'")
-print(cur.fetchone()[0])
